4bd224fdf48feb73d46392732d3f4516e3e13040
UiminaM <142613899+UiminaM@users.noreply.github.com>
2024-07-03 10:25:20 +0300

    3

--- app/src/main/java/summerpractice/hometask/hometask2/FilmRepository.kt
+++ app/src/main/java/summerpractice/hometask/hometask2/FilmRepository.kt
@@ -7,7 +7,7 @@ object FilmRepository {
             id = 0,
             title = "The Shawshank Redemption",
             yearIssue = 1994,
-            pictureUrl = "https://c0.klipartz.com/pngpicture/167/601/gratis-png-tim-robbins-la-redencion-de-shawshank-la-pelicula-de-green-man-morgan-man-youtube.png",
+            pictureUrl = "https://pic.rutubelist.ru/video/ac/a8/aca823155688e35e5a60bc0fe9499c47.jpg",
             information = "Two imprisoned men bond over a number of years, " +
                     "finding solace and eventual redemption through acts of common decency."
         ),
@@ -23,7 +23,7 @@ object FilmRepository {
             id = 2,
             title = "The Terminator",
             yearIssue = 1984,
-            pictureUrl = "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+            pictureUrl = "https://pic.rutubelist.ru/video/85/b7/85b75134c7b23c1df45732018fdf1556.jpg",
             information = "The story of the confrontation between soldier Kyle Reese and cyborg Terminator, "+
                     "who arrived in 1984 from the post-apocalyptic future, where the world is ruled by killer machines, "+
                     "and humanity is on the verge of extinction."
@@ -33,7 +33,7 @@ object FilmRepository {
             id = 3,
             title = "Pulp Fiction",
             yearIssue = 1994,
-            pictureUrl = "https://vertex-art.ru/wp-content/uploads/%D0%9F%D0%BE%D1%81%D1%82%D0%B5%D1%80%D1%8B/01w.jpg",
+            pictureUrl = "https://wallha.com/download/ws/13/FUmJKEzg-wallha.com.jpg",
             information = "The lives of two mob hitmen, a boxer, a gangster and his wife, " +
                     "and a pair of diner bandits intertwine in four tales of violence and redemption."
         ),
@@ -41,7 +41,7 @@ object FilmRepository {
             id = 4,
             title = "Forrest Gump",
             yearIssue = 1994,
-            pictureUrl = "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+            pictureUrl = "https://www.fonstola.ru/images/202009/fonstola.ru_407238.jpg",
             information = "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, " +
                     "and other historical events unfold from the perspective of an Alabama man with an IQ of 75."
         ),
@@ -49,7 +49,7 @@ object FilmRepository {
             id = 5,
             title = "Inception",
             yearIssue = 2010,
-            pictureUrl = "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
+            pictureUrl = "https://i.pinimg.com/originals/ce/27/2c/ce272c7dd4bf3f749971df5209017c8a.jpg",
             information = "A thief who steals corporate secrets through the use of dream-sharing technology " +
                     "is given the inverse task of planting an idea into the mind of a C.E.O."
         ),
@@ -57,7 +57,7 @@ object FilmRepository {
             id = 6,
             title = "The Matrix",
             yearIssue = 1999,
-            pictureUrl = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+            pictureUrl = "https://papik.pro/uploads/posts/2021-10/1634581752_3-papik-pro-p-poster-matritsa-4.jpg",
             information = "A computer hacker learns from mysterious rebels about the true nature of his reality " +
                     "and his role in the war against its controllers."
         )
